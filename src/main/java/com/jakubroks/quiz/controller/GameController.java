8bb308bf00c3b60dc518bf17a3f0ae9ef48fce97
Jakub Roks <kubaroks@onet.eu>
2025-09-04 17:25:59 +0200

    [#6] Quiz Report implemented

--- src/main/java/com/jakubroks/quiz/controller/GameController.java
+++ src/main/java/com/jakubroks/quiz/controller/GameController.java
@@ -1,15 +1,23 @@
 package com.jakubroks.quiz.controller;
 
+import com.jakubroks.quiz.dto.QuestionDTO;
+import com.jakubroks.quiz.dto.QuizResultDTO;
 import com.jakubroks.quiz.entry.GameEntry;
 import com.jakubroks.quiz.input.AnswerInput;
 import com.jakubroks.quiz.input.GameInput;
 import com.jakubroks.quiz.service.GameService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @RestController
 @RequestMapping("/game")
@@ -32,7 +40,30 @@ public class GameController {
     @PostMapping("/answer")
     public ResponseEntity<?> answer(
             @RequestHeader("user") String userId,
-            @RequestBody AnswerInput answerInput) {
-        return ResponseEntity.ok(gameService.submitAnswer(userId, answerInput));
+            @RequestBody AnswerInput answerInput) throws IOException {
+        GameEntry entry = gameService.submitAnswer(userId, answerInput);
+
+        if (entry.questions() != null && entry.answers() != null && entry.score() != null) {
+            QuizResultDTO result = new QuizResultDTO(
+                    entry.id().toString(),
+                    entry.questions().stream()
+                            .map(q -> new QuestionDTO(q.getText(), q.getCorrectAnswer()))
+                            .toList(),
+                    entry.answers(),
+                    entry.score()
+            );
+            byte[] pdfBytes = gameService.generateGameReportPdf(result);
+
+            Path filePath = Path.of("reports", "quiz_report_" + result.id() + ".pdf");
+            Files.createDirectories(filePath.getParent());
+            Files.write(filePath, pdfBytes);
+
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=quiz_report.pdf")
+                    .contentType(MediaType.APPLICATION_PDF)
+                    .body(pdfBytes);
+        } else {
+            return ResponseEntity.ok(entry);
+        }
     }
 }
